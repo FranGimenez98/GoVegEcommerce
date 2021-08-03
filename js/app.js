b1073d1bed5308522c58571b59f2b80aba6d6743
franprogweb98 <franprogweb98@gmail.com>
2021-08-02 21:50:32 -0300

    actualizacion

--- js/app.js
+++ js/app.js
@@ -45,3 +45,11 @@ links.map((link) => {
     document.body.classList.remove("show");
   });
 });
+
+
+function scrollUp(){
+  const scrollUp = document.getElementById('scroll-up');
+  
+  if(this.scrollY >= 560)scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
